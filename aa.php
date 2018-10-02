5e4538c5a07047ebab726d4472e3b8a290457af0
oilugi7 <oilugi7@gmail.com>
2018-10-02 15:46:30 +0900

    no message

---
+++ aa.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Document</title>
+ </head>
+ <body>
+  <?php 
+  $a = "가나다라 $^&((*&하하";
+  echo $a;
+  echo "<br/>";
+  echo urlencode($a);
+  $b = urlencode($a);
+  echo $b;
+  echo "<br/>";
+  echo urldecode($b);
+  ?>
+  aaaaaaaaaaa23
+ </body>
+</html>
